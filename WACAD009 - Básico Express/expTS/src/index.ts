9f3cb64eb90e70f61e46eafce81c22de6c8c5735
kauasales <kaua.salesviana@gmail.com>
2026-06-06 09:47:14 -0400

    Implementando helper handlebars

--- WACAD009 - Básico Express/expTS/src/index.ts
+++ WACAD009 - Básico Express/expTS/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import routes from './routes/routes.js';
 import { logger } from './middleware/logger.js';
 import { engine } from 'express-handlebars';
+import { Technology } from './interfaces/Technology.js';
 import path from 'path';
 
 dotenv.config();
@@ -12,7 +13,27 @@ const PORT = Number(process.env.PORT) || 3333;
 
 app.use(logger('completo'));
 
-app.engine('handlebars', engine());
+app.engine(
+  'handlebars',
+  engine({
+    helpers: {
+      nodeTechnologies: (technologies) => {
+        let result = '<ul>';
+
+        technologies
+          .filter((tech: Technology) => tech.poweredByNodejs)
+          .forEach((tech: Technology) => {
+            result += `<li>${tech.name} (${tech.type})</li>`;
+          });
+
+        result += '</ul>';
+
+        return result;
+      },
+    },
+  })
+);
+
 app.set('view engine', 'handlebars');
 
 app.set('views', path.join(process.cwd(), 'src', 'views'));
