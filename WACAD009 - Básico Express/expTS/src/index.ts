36403bccfda184644db40323ce444e2fafd9a81b
kauasales <kaua.salesviana@gmail.com>
2026-06-06 10:45:14 -0400

    Configuração do SASS

--- WACAD009 - Básico Express/expTS/src/index.ts
+++ WACAD009 - Básico Express/expTS/src/index.ts
@@ -1,24 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { engine } from 'express-handlebars';
-import path from 'path';
-
 import routes from './routes/routes.js';
+import { logger } from './middleware/logger.js';
+import { engine } from 'express-handlebars';
+import { Technology } from './interfaces/Technology.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+const PORT = Number(process.env.PORT) || 3333;
+
+app.use(express.static(path.join(process.cwd(), 'src/public')));
+app.use(logger('completo'));
 
 app.engine(
   'handlebars',
   engine({
     helpers: {
-      nodeTechnologies: (technologies: any[]) => {
+      nodeTechnologies: (technologies) => {
         let result = '<ul>';
 
         technologies
-          .filter((tech) => tech.poweredByNodejs)
-          .forEach((tech) => {
+          .filter((tech: Technology) => tech.poweredByNodejs)
+          .forEach((tech: Technology) => {
             result += `<li>${tech.name} (${tech.type})</li>`;
           });
 
@@ -27,17 +32,15 @@ app.engine(
         return result;
       },
     },
-  }),
+  })
 );
 
 app.set('view engine', 'handlebars');
 
-app.set('views', path.join(process.cwd(), 'src/views'));
+app.set('views', path.join(process.cwd(), 'src', 'views'));
 
 app.use(routes);
 
-const PORT = Number(process.env.PORT) || 3333;
-
 app.listen(PORT, () => {
-  console.log(`Servidor iniciado na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
