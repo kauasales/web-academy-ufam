1ba6a4b95f66aa427f23379edbbbeb4000f36fe7
kauasales <kaua.salesviana@gmail.com>
2026-07-22 21:05:07 -0400

    Reapply "useQuery - api/produto"
    This reverts commit 479f3ac999e88530a4f5a5544624c2bd542e2d08.

--- WACAD014 - Biblioteca Frontend REACT/wa-loja-react-next/src/app/page.tsx
+++ WACAD014 - Biblioteca Frontend REACT/wa-loja-react-next/src/app/page.tsx
@@ -1,19 +1,27 @@
 'use client'
 import { useState } from 'react'
-import Navbar from './components/Navbar/Navbar'
 import CartSummary from './components/CartSummary/CartSummary'
 import ProductList from './components/ProductList/ProductList'
-import { mockProducts } from './mocks/products'
 import { Product } from './types/product'
+import { useQuery } from '@tanstack/react-query'
+import { getProducts } from './services/api'
 
 export default function Products() {
   const [totalQuantity, setTotalQuantity] = useState<number>(0)
   const [totalValue, setTotalValue] = useState<number>(0)
 
+  const { data: products, isLoading, error } = useQuery<Product[]>({
+    queryKey: ['products'],
+    queryFn: getProducts
+  })
+
   const addToCart = (product: Product) => {
     setTotalQuantity((prev) => prev + 1)
-    setTotalValue((prev) => prev + Number(product.price))
+    setTotalValue((prev) => prev + Number(product.preco))
   }
+
+  if (isLoading) return <div className='container p-5'>Carregando produtos...</div>
+  if (error) return <div className='container p-5'>Erro ao carregar produtos.</div>
 
   return (
     <>
@@ -23,9 +31,10 @@ export default function Products() {
 
           <h5 className='mb-3'>Produtos disponíveis:</h5>
 
-          <ProductList products={mockProducts} onAddToCart={addToCart} />
+          {products && <ProductList products={products} onAddToCart={addToCart} />}
         </div>
       </main>
     </>
   )
 }
+
